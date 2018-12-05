08573bf6ab16a95d5207ee8957a24876416d197c
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2018-12-05 10:24:40 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-from jxmap import __version__ as VERSION
+from jxmap._version import __version__ as VERSION
 
 setup(name='jxmap',
       version=VERSION,
